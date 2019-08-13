d45362025f16b0b90277349a6f2710dae394bf95
Sean Takats <sean@takats.org>
2019-08-13 11:49:45 -0400

    Initial commit of filter; add usage to README

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # zotero-cv-to-markdown
 Panflute (Pandoc python) filter that converts a zotero.org CV to markdown, e.g. for a Jekyll blog page
+
+Usage:
+```shell
+pandoc -f html -t markdown-raw_html-native_divs-fenced_divs-header_attributes https://zotero.org/{username}/cv/ --filter zotero-cv-to-markdown.py -s -o cv.md
+```
+
---
+++ zotero-cv-to-markdown.py
@@ -0,0 +1,37 @@
+"""
+Panflute filter that converts a zotero.org CV to markdown for a Jekyll blog page
+Usage:
+	pandoc -f html -t markdown-raw_html-native_divs-fenced_divs-header_attributes https://zotero.org/{username}/cv/ --filter zotero-cv-to-markdown.py -s -o cv.md
+"""
+
+import panflute
+import datetime
+
+def prepare(doc):
+	doc.entries = []
+
+def action(elem, doc):
+	if isinstance(elem, panflute.Div):
+		if "profile_cvEntry" in elem.classes:
+			doc.entries.append(elem)
+
+def finalize(doc):
+	temp_entries = doc.entries
+	doc.content = []
+
+	now = datetime.datetime.now(datetime.timezone.utc)
+	datestamp = now.astimezone().isoformat('T', 'seconds')
+
+	# customize the YAML below as desired for the generated CV
+	doc.metadata = {'title':'CV', 'date':datestamp, 
+	'layout':'page', 'custom_css': 'cv', 'permalink':'/cv/'
+		}
+
+	for entry in temp_entries:
+		doc.content.append(entry)
+
+def main(doc=None):
+    return panflute.run_filter(action, prepare=prepare, finalize=finalize, doc=doc)
+
+if __name__ == "__main__":
+    main()
